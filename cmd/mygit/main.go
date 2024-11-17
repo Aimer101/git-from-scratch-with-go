80c2d66cbfcb77391cf5e8684274559029477999
Muaz Zakaria <muazzakaria@MacBook-Air-von-MacSzerezcom.local>
2024-11-17 13:56:01 +0100

    - handle git cat-file cmd

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-// Usage: your_program.sh <command> <arg1> <arg2> ...
 func main() {
 
 	if len(os.Args) < 2 {
@@ -25,6 +28,42 @@ func main() {
 			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
 		}
 		fmt.Println("Initialized git directory")
+
+	case "cat-file":
+		sha := os.Args[3]
+
+		// first 2 of sha is dir
+		// 2: is the file
+
+		fullPath := filepath.Join(".git/objects", sha[:2], sha[2:])
+
+		file, err := os.Open(fullPath)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
+		}
+
+		decompressor, err := zlib.NewReader(file) // create decompressor
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
+		}
+
+		r, err := io.ReadAll(decompressor) // read decompressed data
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
+		}
+
+		decompressor.Close()
+		// fmt.Println(string(r))
+
+		res := strings.Split(string(r), "\x00")
+
+		// for index, line := range res {
+		// 	fmt.Println(index, line)
+		// }
+		fmt.Print(res[1])
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
